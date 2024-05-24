1a51e7ece2b22f7db777dacdacee048787b05e05
Francilene Silva <psilva.francilene@gmail.com>
2024-05-24 16:43:53 +0100

    testes de task duplicada via

---
+++ README.md
@@ -0,0 +1,9 @@
+### Sobre 📝
+repositório do projeto de testes automatizados de API do sistema Mark85, feito no curso de Teste de API REST.
+
+### Tecnologias 🛠️
+* Cypress
+* MongoDB
+* RabbitMQ
+* Javascript (Node)
+
--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -17,15 +17,38 @@ describe('POST /tasks', () => {
 
         // fazendo login
         cy.postSession(user)
-            .then(response => {
+            .then(userResp => {
                 // excluindo a task caso já exista
                 cy.task('deleteTask', task.name, user.email)
 
-                cy.postTask(task, response.body.toke)
+                cy.postTask(task, userResp.body.token)
                     .then(response => {
                         expect(response.status).to.eq(200)
                         expect(response.body.name).to.eq(task.name)
-                        expect(response.body.tags).to.eq(task.tags)
+                        expect(response.body.tags).to.eql(task.tags)
+                        expect(response.body.is_done).to.be.false
+                        expect(response.body.user).to.eq(userResp.body.user._id)
+                        expect(response.body._id.length).to.eq(24)
+                    })
+
+            })
+    })
+
+    it('duplicate task', function () {
+        const { user, task } = this.tasks.dup
+
+        // deletando e criando o usuário
+        cy.task('deleteUser', user.email)
+        cy.postUser(user)
+
+        // fazendo login
+        cy.postSession(user)
+            .then(userResp => {
+                cy.postTask(task, userResp.body.token)
+                cy.postTask(task, userResp.body.token)
+                    .then(response => {
+                        expect(response.status).to.eq(409)
+                        expect(response.body.message).to.eq('Duplicated task!')
                     })
 
             })
